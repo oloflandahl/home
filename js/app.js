74947fcb35f7bb62afa79495a3f587961921158c
oloflandahl <oloflandahl@hotmail.com>
2013-10-21 22:27:27 +1100

    added touch effect support

--- js/app.js
+++ js/app.js
@@ -1,179 +1,193 @@
-$(document).ready(function() {
-
+(function() {
 	'use strict';
 
-	var activeGroup = (function() {
+	var addTouchHandler = function(el) {
+		el.on('touchstart', function() { $(this).addClass('touching') });
+		el.on('touchend', function() { $(this).removeClass('touching') });
+	};
 
-		var	offset,
-			index = 0, 
-			win = $(window),
-			body = $('body'),
-			menu = $('.menu'),
-			groups = $('.group'),
-			arrow = $('.arrow'),
-			arrowUp = arrow.filter('.up'),
-			arrowDown = arrow.filter('.down'),
-			bounds = { min: 0, max: groups.length-1 };
+	$(document).ready(function() {
 
-		setOffset();
+		addTouchHandler($('.group li'));
+		addTouchHandler($('.links li'));
+		addTouchHandler($('.contact'));
+		addTouchHandler($('.arrow'));
 
-		function setOffset() {
-			offset = menu.outerHeight();
-		};
+		var activeGroup = (function() {
 
-		var scrollToActiveGroup = function() {
-			var pos = groups.eq(index).offset().top;
-			$('body, html').animate({scrollTop: (pos - offset) }, 600, 'swing');			
-		};
+			var	offset,
+				index = 0, 
+				win = $(window),
+				body = $('body'),
+				menu = $('.menu'),
+				groups = $('.group'),
+				arrow = $('.arrow'),
+				arrowUp = arrow.filter('.up'),
+				arrowDown = arrow.filter('.down'),
+				bounds = { min: 0, max: groups.length-1 };
 
-		var setActiveLink = function() {
-			menu.find('.nav-link.active').removeClass('active');
-			menu.find('.nav-link').eq(index).addClass('active');
-		};
+			setOffset();
 
-		var updateArrows = function() {
-			if (index === bounds.min) {
-				arrowUp.addClass('hide');
-				arrowDown.removeClass('hide');
-			}
-			else if (index === bounds.max) {
-				arrowDown.addClass('hide');
-				arrowUp.removeClass('hide');
-			}
-			else {
-				arrow.removeClass('hide');
-			}
-		};
+			function setOffset() {
+				offset = menu.outerHeight();
+			};
 
-		var setIndex = function(newIndex, scroll) {
+			var scrollToActiveGroup = function() {
+				var pos = groups.eq(index).offset().top;
+				$('body, html').animate({scrollTop: (pos - offset) }, 600, 'swing');			
+			};
 
-			if (newIndex < bounds.min || newIndex > bounds.max) {
-				console.error('Invalid index ' + newIndex);
-				return;
-			}
+			var setActiveLink = function() {
+				menu.find('.nav-link.active').removeClass('active');
+				menu.find('.nav-link').eq(index).addClass('active');
+			};
 
-			if (index !== newIndex) {
-				index = newIndex;
-				setActiveLink();
-				updateArrows();
-
-				if (scroll) {
-					scrollToActiveGroup();
+			var updateArrows = function() {
+				if (index === bounds.min) {
+					arrowUp.addClass('hide');
+					arrowDown.removeClass('hide');
 				}
-			}
-		};
+				else if (index === bounds.max) {
+					arrowDown.addClass('hide');
+					arrowUp.removeClass('hide');
+				}
+				else {
+					arrow.removeClass('hide');
+				}
+			};
 
-		var updateActive = function() {
-			var i = 0;
-			groups.each(function() {
-				var groupPos = $(this).offset().top,
-					scrollPos = win.scrollTop();
-				if (groupPos + offset > scrollPos) { return false }
-				else { i++ }
+			var setIndex = function(newIndex, scroll) {
+
+				if (newIndex < bounds.min || newIndex > bounds.max) {
+					console.error('Invalid index ' + newIndex);
+					return;
+				}
+
+				if (index !== newIndex) {
+					index = newIndex;
+					setActiveLink();
+					updateArrows();
+
+					if (scroll) {
+						scrollToActiveGroup();
+					}
+				}
+			};
+
+			var updateActive = function() {
+				var i = 0;
+				groups.each(function() {
+					var groupPos = $(this).offset().top,
+						scrollPos = win.scrollTop();
+					if (groupPos + offset > scrollPos) { return false }
+					else { i++ }
+				});
+				setIndex(i, false);
+			};
+
+			arrowUp.click(function() { activeGroup.moveUp() });
+			arrowDown.click(function() { activeGroup.moveDown() });
+
+			var scrolling;
+			win.scroll(function() {
+				if (scrolling) {
+					clearTimeout(scrolling);
+				}
+				scrolling = setTimeout(updateActive, 200);
 			});
-			setIndex(i, false);
-		};
 
-		arrowUp.click(function() { activeGroup.moveUp() });
-		arrowDown.click(function() { activeGroup.moveDown() });
+			var resizing;
+			win.resize(function() {
+				if (resizing) {
+					clearTimeout(resizing);
+				}
+				resizing = setTimeout(setOffset, 200);
+			});
 
-		var scrolling;
-		win.scroll(function() {
-			if (scrolling) {
-				clearTimeout(scrolling);
-			}
-			scrolling = setTimeout(updateActive, 200);
-		});
+			return {
+				moveDown: function() { setIndex(index+1, true) },
+				moveUp: function() { setIndex(index-1, true) },
+				moveTo: function(newIndex) { setIndex(newIndex, true) }
+			};
 
-		var resizing;
-		win.resize(function() {
-			if (resizing) {
-				clearTimeout(resizing);
-			}
-			resizing = setTimeout(setOffset, 200);
-		});
+		}());
 
-		return {
-			moveDown: function() { setIndex(index+1, true) },
-			moveUp: function() { setIndex(index-1, true) },
-			moveTo: function(newIndex) { setIndex(newIndex, true) }
-		};
+		var linksMenu = (function() {
 
-	}());
+			var SEL = {
+				menu: '.menu',
+				links: '.links',
+				navLink: '.nav-link'
+			};
 
-	var linksMenu = (function() {
+			var doc = $(document),
+				win = $(window),
+				menu = $(SEL.menu),
+				links = menu.find(SEL.links),
+				linksList = links.find('ul'); 
 
-		var SEL = {
-			menu: '.menu',
-			links: '.links',
-			navLink: '.nav-link'
-		};
+			menu.on('click', SEL.navLink, function() { activeGroup.moveTo($(this).index()) });
 
-		var doc = $(document),
-			win = $(window),
-			menu = $(SEL.menu),
-			links = menu.find(SEL.links),
-			linksList = links.find('ul'); 
+			menu.on('click', SEL.links, function(e) {
+				if (this !== e.target) {
+					return true;
+				}
 
-		menu.on('click', SEL.navLink, function() { activeGroup.moveTo($(this).index()) });
+				if (linksList.is(':hidden')) {
+					links.addClass('on');
+					linksList.fadeIn(400);
+				}
+				else if(links.is('.on')) {
+					links.removeClass('on');
+					linksList.fadeOut(400);
+				}
 
-		menu.on('click', SEL.links, function(e) {
-			if (this !== e.target) {
-				return true;
-			}
+				return false;
+			});
 
-			if (linksList.is(':hidden')) {
-				links.addClass('on');
-				linksList.fadeIn(400);
-			}
-			else if(links.is('.on')) {
-				links.removeClass('on');
-				linksList.fadeOut(400);
-			}
+			doc.click(function(e) {
 
-			return false;
-		});
+				if (!links.is('.on')) {
+					return true;
+				}
 
-		doc.click(function(e) {
+				var clickedLink = $(e.target).filter(SEL.navLink)
+					.add($(e.target).closest(SEL.navLink));
 
-			if (!links.is('.on')) {
-				return true;
-			}
+				if (clickedLink.length === 0) {
+					linksList.removeClass('on');
+					linksList.fadeOut(400);
+				}
+			});
 
-			var clickedLink = $(e.target).filter(SEL.navLink)
-				.add($(e.target).closest(SEL.navLink));
+			var toggleLinks = function() {
+				if (links.css('cursor') !== 'pointer') {
+					// Narrow screen.
+					linksList.show();
+					links.removeClass('on');
+				}
+				else if (!links.is('.on')) {
+					linksList.hide();
+				}
+			};
 
-			if (clickedLink.length === 0) {
-				linksList.removeClass('on');
-				linksList.fadeOut(400);
-			}
-		});
+			links.hover(function(e) {
+				if (e.target === this) {
+					$(this).addClass('hover');
+				}
+			}, function(e) {
+				$(this).removeClass('hover');
+			});
 
-		var toggleLinks = function() {
-			if (links.css('cursor') !== 'pointer') {
-				linksList.show();
-				links.removeClass('on');
-			}
-			else if (!links.is('.on')) {
-				linksList.hide();
-			}
-		};
+			var resizing;
+			win.resize(function() {
+				if (resizing) {
+					clearTimeout(resizing);
+				}
+				resizing = setTimeout(toggleLinks, 200);
+			});
+		}());
 
-		links.hover(function(e) {
-			if (e.target === this) {
-				$(this).addClass('hover');
-			}
-		}, function(e) {
-			$(this).removeClass('hover');
-		});
+	});
 
-		var resizing;
-		win.resize(function() {
-			if (resizing) {
-				clearTimeout(resizing);
-			}
-			resizing = setTimeout(toggleLinks, 200);
-		});
-	}());
-
-});
+}());
